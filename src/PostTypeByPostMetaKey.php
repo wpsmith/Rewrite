b8de8de5a54a55bfdb8473901395d27f692c8775
wpsmith <t@wpsmith.net>
2019-02-20 20:53:39 -0500

    Update namespace/docs

--- src/PostTypeByPostMetaKey.php
+++ src/PostTypeByPostMetaKey.php
@@ -10,21 +10,21 @@
  *
  * @package    WPS\Rewrite
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Rewrite;
+namespace WPS\WP\Rewrite;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Rewrite\PostTypeByPostMetaKey' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\PostTypeByPostMetaKey' ) ) {
 	/**
 	 * Class PostTypeByPostMeta
 	 *
